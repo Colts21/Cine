d4fced118b96fe27e58a4b55d5af2c4bcbe5b539
Donovan <donogf@hotmail.fr>
2021-02-04 13:32:44 +0100

    CRUD Film

--- src/main/java/org/cnam/sample/entity/Film.java
+++ src/main/java/org/cnam/sample/entity/Film.java
@@ -16,4 +16,42 @@ public class Film {
     private String genre;
     private String realisateur;
 
+    public Film(int id, String name, String genre, String realisateur) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.realisateur = realisateur;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getRealisateur() {
+        return realisateur;
+    }
+
+    public void setRealisateur(String realisateur) {
+        this.realisateur = realisateur;
+    }
 }
--- src/main/java/org/cnam/sample/repository/FilmRepository.java
+++ src/main/java/org/cnam/sample/repository/FilmRepository.java
@@ -2,6 +2,9 @@ package org.cnam.sample.repository;
 
 import org.cnam.sample.entity.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FilmRepository extends JpaRepository<Film,Integer> {
+    Film findByName(String name);
 }
